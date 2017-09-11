585028f55107123d3555406767449c9229ecb34a
best2409 <best2409@naver.com>
2017-09-11 16:31:38 +0900

    Test

---
+++ 01.BasicProject/src/dmirae/a/datatype/DataTypeTest.java
@@ -0,0 +1,10 @@
+package dmirae.a.datatype;
+
+public class DataTypeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
